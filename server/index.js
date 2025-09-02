bedc5a197fa26d04cc33f9dae3918adb7eadde58
NuwaNian <nuwa.ninian@gmail.com>
2025-09-02 10:19:31 +0700

    Resolve

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
